d2921e2af824eb7193e3ac6a32abb8b59fb94b1d
molly <mollynzembi@gmail.com>
2024-03-25 15:24:27 +0300

    creates inde.js

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ return (`This Saturday, I want to ${activity}!`);
 function mondayWork(activity = 'go to the office') {
     return (`This Monday, I will ${activity}.`);
 }
-function wrapAdjective(wrapper = '*') {
-    return function(adjective) {
-        return `You are ${wrapper}${adjective}${wrapper}!`;
+function wrapAdjective(star = "*") {
+    return function(adjective = "special") {
+        return `You are ${star}${adjective}${star}!`;
     };
 }
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+
+console.log(encouragingPromptFunction("a dedicated programmer"));
